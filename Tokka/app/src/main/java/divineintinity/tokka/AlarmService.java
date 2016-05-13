55de8d9860504512b1200b8a1ad408ad0aca172f
Dushan Chanuka <dushancc@gmail.com>
2016-05-13 09:06:00 +0530

    Alarm Service and Reciever Added

--- Tokka/app/src/main/java/divineintinity/tokka/AlarmService.java
+++ Tokka/app/src/main/java/divineintinity/tokka/AlarmService.java
@@ -1,7 +1,17 @@
 package divineintinity.tokka;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Xeon on 5/13/2016.
  */
-public class AlarmService {
+public class AlarmService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
---
+++ Tokka/app/src/main/java/divineintinity/tokka/Alarm_Reciever.java
@@ -0,0 +1,19 @@
+package divineintinity.tokka;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Xeon on 5/13/2016.
+ */
+public class Alarm_Reciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+
+
+}
